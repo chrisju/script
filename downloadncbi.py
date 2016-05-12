4ec41be829acae23babd6fc091fb2ef6697e11c5
chrisju <zhuzhu101011@gmail.com>
2016-05-12 17:16:10 +0800

    fix downloadncbi

--- downloadncbi.py
+++ downloadncbi.py
@@ -10,11 +10,12 @@ if len(sys.argv) < 2:
     print 'example: %s SRR_Acc_List.txt' % (sys.argv[0])
 
 def getsrr(ac):
+    ac = ac.strip(' \t\r\n')
     fmt = 'ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/%s/%s/%s/%s.sra'
     url = fmt % (ac[:3],ac[:6],ac,ac)
-    print('downloading',url)
+    print('downloading '+url)
     #cmd = 'wget -c %s -O %s.sra' % (url,ac)
-    cmd = 'axel -n 1 -s 800000 %s -o %s.sra  ' % (url,ac)
+    cmd = 'axel -a -n 1 -s 800000 %s -o %s.sra  ' % (url,ac)
     print cmd
     os.system(cmd)
 
