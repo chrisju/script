e935d04c0b99403fb6c44e460a0a81ff4b202e7b
chrisju <zhuzhu101011@gmail.com>
2016-07-20 19:57:27 +0800

    1

--- startup.sh
+++ startup.sh
@@ -7,5 +7,6 @@ nohup thunderbird >/dev/null 2>&1 &
 nohup pidgin >/dev/null 2>&1 &
 nohup /opt/longene/tm2013/tm2013.sh >/dev/null 2>&1 &
 cd /mnt/DATA/upload;nohup python /home/zz/script/SimpleHTTPServerWithUpload.py >/dev/null 2>&1 &
-sslocal -c /etc/shadowsocks/config-puff.json
+nohup sslocal -c /etc/shadowsocks/config-puff.json >/dev/null 2>&1 &
+exit 0
 
